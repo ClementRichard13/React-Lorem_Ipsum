f80a6550c3057d3e93d4d00213c2dced2280db1a
Clément Richard <clementrichard@macbook-pro-de-clement.home>
2023-12-16 17:26:09 +0100

    first-step-lorem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <div className="App">
+      <h1>Lorem</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet saepe
+        obcaecati dolores similique voluptatum a error nulla ipsa, eaque
+        blanditiis commodi mollitia beatae iste ratione labore omnis, iusto
+        aliquam minus?
+      </p>
+    </div>
+  );
 }
 
 export default App;
